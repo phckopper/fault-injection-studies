8ff8ef0616086027882797ac56b9ded13b8826ea
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2020-03-18 16:52:26 -0300

    use prime numbers to benchmark injector

--- examples/main.c
+++ examples/main.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
+#include <math.h>
 
-int math(int a, int b) {
-    int c = a + b;
-    c++;
-    if(c > 10)
-        c = 2;
-    c = c * a;
-    c = c / b;
-    if(a % 2 == 0)
-        c = c + a + b;
-    return c;
+#define N 99
+
+void print_if_prime(int n) {
+   int i = 3;
+
+   if(n % 2 == 0) {
+       printf("%d NOT PRIME\n", n);
+       return;
+   }
+
+   while(i < sqrt(n)) {
+        if(n % i == 0) {
+            printf("%d NOT PRIME\n", n);
+            return;
+        }
+        i += 2;
+   }
+   printf("%d PRIME\n", n);
 }
 
 int main() {
-    int a = 3, b = 3;
+    for(int i = 0; i < N; ++i)
+        print_if_prime(i);
     
-    int c = math(a, b);
-    printf("%d", c);
+    return 0;
 }

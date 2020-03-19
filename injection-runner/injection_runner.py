95c60572e09f3eedd6cecaa5ecf4a79982a323c9
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2020-03-19 16:21:01 -0300

    start using click for cli

--- injection-runner/injection_runner.py
+++ injection-runner/injection_runner.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
-from sys import argv
+import click
 from utils.Campaign import Campaign
 
-c = Campaign(argv[1], argv[2])
-c.run()
+@click.group()
+def cli():
+    pass
 
+@cli.command()
+@click.argument('executable')
+@click.argument('address_map')
+@click.option('--tolerance', default=5, help='how many times slower should a run be to be a hang')
+def run_campaign(executable, address_map, tolerance):
+    c = Campaign(executable, address_map, tolerance)
+    c.run()
+
+if __name__ == '__main__':
+    cli()

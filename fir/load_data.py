0329f6c5afbe89c00eeaca435dd6d81866eab870
Pedro Henrique Kopper <phckopper@inf.ufrgs.br>
2020-09-11 12:24:26 -0300

    add corrected fir data (truncation error)

--- fir/load_data.py
+++ fir/load_data.py
@@ -2,8 +2,14 @@ LEN = 8000
 CAP = 2**23 - 1
 
 clamp = lambda n: max(min(CAP, n), -CAP)
+def getSignedNumber(number, bitLength):
+    mask = (2 ** bitLength) - 1
+    if number & (1 << (bitLength - 1)):
+        return number | ~mask
+    else:
+        return number & mask
 
 def load_output(name):
         with open(name, "r") as f:
-                    return [clamp(int(line)) for line in f]
+                    return [getSignedNumber(int(line), 23) for line in f]
 
